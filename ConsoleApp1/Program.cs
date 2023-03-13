c1801b2f2e721acf42cf62422aa380f4bbd3a20f
wolekw <wolekw@P21713>
2023-03-13 11:31:29 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BankAccountNS
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class StringCalculator
+    {
+
+
+        public int Calculate(string arg)
+        {
+            List<string> res = new List<string>();
+            int sum;
+            if (string.IsNullOrEmpty(arg))
+            {
+                return 0;
+            }
+            if(arg.Length>3 &&arg.Substring(0,2)=="\\\\")
+            {
+                res = arg.Substring(2).Split(arg[2]).ToList();
+            }
+            
+            if (int.TryParse(arg, out int result))
+            {
+                res.Add(arg);
+            }
+            if (Regex.IsMatch(arg, "([0-9]+\n)+[0-9]+"))
+            {
+                res = arg.Split('\n').ToList();
+            }
+            if (Regex.IsMatch(arg, "([0-9]+,)+[0-9]+"))
+            {
+                res = arg.Split(',').ToList();
+            }
+            sum = 0;
+            foreach (var n in res)
+            {
+                if (int.TryParse(n, out int numb))
+                {
+                    if(numb < 0)
+                    {
+                        throw new Exception();
+                    }
+                    else if (numb >= 1000)
+                    {
+                        continue;
+                    }
+                    sum += numb;
+                }
+               
+            }
+            return sum;
+
+        }
+
+
+        public static void Main()
+        {
+            String s = "-1";
+            StringCalculator stringCalculator = new StringCalculator();
+            Console.WriteLine("String s" + s + "calculated" + stringCalculator.Calculate(s));
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+using BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod_Empty()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(0, s.Calculate(""));
+            
+        }
+        [TestMethod]
+        public void TestMethod_SingleNumber()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(12, s.Calculate("12"));
+            Assert.AreEqual(58, s.Calculate("58"));
+
+        }
+        [TestMethod]
+        public void TestMethod_2NumbersComa()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(2,s.Calculate("1,1")); 
+            Assert.AreEqual(40,s.Calculate("10,30"));
+
+        }
+        
+        [TestMethod]
+        public void TestMethod_2NumbersNewLine()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(40,s.Calculate("10\n30"));
+
+        }
+       
+        [TestMethod]
+        public void TestMethod_3Numbers()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(42, s.Calculate("10,30,2"));
+            Assert.AreEqual(42, s.Calculate("10\n30\n2"));
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestMethod_Negative()
+        {
+            var s = new StringCalculator();
+
+            s.Calculate("-1");
+
+        }
+        [TestMethod]
+        public void TestMethod_MoreThan1000()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(11, s.Calculate("1000\n10\n1"));
+        }
+        [TestMethod]
+        public void TestMethod_CustomSplit()
+        {
+            var s = new StringCalculator();
+            Assert.AreEqual(5, s.Calculate("\\\\$2$3"));
+        }
+    }
+}
